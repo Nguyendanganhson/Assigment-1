60595a96c4199869b624027611c109f289c082f6
Nguyen Son <anhsonnd04@gmail.com>
2022-05-12 23:54:19 +0900

    sửa ngày 12/5

---
+++ src/components/ComponentsStaffList.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { STAFFS } from "./shared/staffs";
+import "./StaffList.css";
+function StaffList() {
+  const memberInfo = (i) => {
+    document.getElementById("title").innerHTML = "";
+  };
+
+  return (
+    <div id="wapper">
+      {/*Tạo top của trang*/}
+      <div id="header">
+        <h3>Ứng dụng quản lý nhân sự v1.0</h3>
+      </div>
+      {/*hiển thị list nhân viên*/}
+      <div id="row">
+        <div className="col-lg-4 col-md-6 col-12" id="item-1">
+          <p onClick={() => memberInfo("thong bao")}>{STAFFS[0].name}</p>
+        </div>
+
+        <div className="col-lg-4 col-md-6 col-12">
+          <p>{STAFFS[1].name}</p>
+        </div>
+        <div className="col-lg-4 col-md-6 col-12">
+          <p>{STAFFS[2].name}</p>
+        </div>
+        <div className="col-lg-4 col-md-6 col-12">
+          <p>{STAFFS[3].name}</p>
+        </div>
+        <div className="col-lg-4 col-md-6 col-12">
+          <p>{STAFFS[4].name}</p>
+        </div>
+        <div className="col-lg-4 col-md-6 col-12">
+          <p>{STAFFS[5].name}</p>
+        </div>
+      </div>
+      {/*Hiển thị thông tin chi tiết*/}
+      <div id="detai" className="col-lg-4 col-md-6 col-12">
+        <p id="title">
+          <strong>Bấm vào tên nhân viên để xem thông tin</strong>
+        </p>
+      </div>
+    </div>
+  );
+}
+export default StaffList;
