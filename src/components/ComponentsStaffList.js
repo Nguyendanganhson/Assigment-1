9f9f8f0107b1035ae66e1d1aaa883125ac9f0b52
Nguyendanganhson <104874617+Nguyendanganhson@users.noreply.github.com>
2022-05-15 15:22:05 +0900

    sua phan list

--- src/components/ComponentsStaffList.js
+++ src/components/ComponentsStaffList.js
@@ -9,38 +9,24 @@ class StaffList extends Component {
   }
 
   render() {
+    var newSTAFFS = STAFFS.splice(0, 6);
+    const listname = newSTAFFS.map((list) => {
+      return (
+        <div className="col-lg-4 col-md-6 col-12" id="row" key={list.id}>
+          <div id="item-1">
+            <p>{list.name}</p>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div id="wapper">
         {/*Tạo header của trang*/}
         <div id="header">
           <h3>Ứng dụng quản lý nhân sự v1.0</h3>
         </div>
-        {/*hiển thị list nhân viên*/}
-        <div id="row">
-          <div className="col-lg-4 col-md-6 col-12" id="item-1">
-            <p>{STAFFS[0].name}</p>
-          </div>
-
-          {/* <div className="col-lg-4 col-md-6 col-12">
-            <p>{STAFFS[1].name}</p>
-          </div>
-
-          <div className="col-lg-4 col-md-6 col-12">
-            <p>{STAFFS[2].name}</p>
-          </div>
-
-          <div className="col-lg-4 col-md-6 col-12">
-            <p>{STAFFS[3].name}</p>
-          </div>
-
-          <div className="col-lg-4 col-md-6 col-12">
-            <p>{STAFFS[4].name}</p>
-          </div>
-
-          <div className="col-lg-4 col-md-6 col-12">
-            <p>{STAFFS[5].name}</p>
-          </div>*/}
-        </div>
+        <div>{listname}</div>
       </div>
     );
   }
