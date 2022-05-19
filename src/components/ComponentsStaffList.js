2a313492194bacf44b8a6b69def9ac090854ef45
Nguyendanganhson <104874617+Nguyendanganhson@users.noreply.github.com>
2022-05-20 00:53:34 +0900

    update 20/05 lan 2

--- src/components/ComponentsStaffList.js
+++ src/components/ComponentsStaffList.js
@@ -6,8 +6,7 @@ import "./StaffList.css";
 class StaffList extends Component {
   constructor(props) {
     super(props);
-    {
-    }
+
     this.state = { status: null };
   }
   memberSelected(list) {
