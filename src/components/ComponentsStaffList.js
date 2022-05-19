03579fa75c6820d971fd390e76580bffe28e5943
Nguyendanganhson <104874617+Nguyendanganhson@users.noreply.github.com>
2022-05-20 01:06:33 +0900

    doi ten barnch

--- src/components/ComponentsStaffList.js
+++ src/components/ComponentsStaffList.js
@@ -60,40 +60,6 @@ export default StaffList;
 
 {
   /* 
-function StaffList() { phiên bản 1.1
-  return (
-    <div id="wapper">
-      
-      <div id="header">
-        <h3>Ứng dụng quản lý nhân sự v1.0</h3>
-      </div>
-      
-      <div id="row">
-        <div className="col-lg-4 col-md-6 col-12" id="item-1">
-          <p>{STAFFS[0].name}</p>
-        </div>
 
-        <div className="col-lg-4 col-md-6 col-12">
-          <p>{STAFFS[1].name}</p>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <p>{STAFFS[2].name}</p>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <p>{STAFFS[3].name}</p>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <p>{STAFFS[4].name}</p>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <p>{STAFFS[5].name}</p>
-        </div>
-      </div>
-     
-      <div id="detai" className="col-lg-4 col-md-6 col-12"></div>
-    </div>
-  );
-}
-export default StaffList;
 */
 }
